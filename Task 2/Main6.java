d0ac5077cab137d4d19963291ecd973a33fc0b53
Amiyo Bushon Chakraborty <amiyochakrabortyff@gmail.com>
2026-06-18 16:53:19 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,25 @@
 
+class Applet{
+    void launch(){
+        System.out.println("Applet lonched");
+
+    }
+}
+
+class GameApplet extends Applet{
+    void play(){
+        System.out.println("playing game!");
+
+    }
+}
+
+
+public class Main6 {
+    public static void main(String[] args){
+       GameApplet game = new GameApplet();
+
+       game.launch();
+       game.play();
+    }
+    
+}
