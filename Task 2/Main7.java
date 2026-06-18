64a43aad9991e49f5eef3837ca8863c76c0556a5
Amiyo Bushon Chakraborty <amiyochakrabortyff@gmail.com>
2026-06-18 16:53:39 +0600

    Implement Clickable interface with Button class

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,21 @@
+interface Clickble{
+    void  onClick();
+}
+class Button implements Clickble{
+    @Override
+    public  void onClick() {
+        System.out.println("Button was clicked!");
+
+    }
+}
+
+
+
+public class Main7 {
+    public static void main(String[] args){
+        Button btn = new Button();
+
+        btn.onClick();
+    }
+}
 
