195746846a91fa677d52fc8c1753a8c4b2692e53
Amiyo Bushon Chakraborty <amiyochakrabortyff@gmail.com>
2026-06-18 16:52:43 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,26 @@
+class Sensor{
+    String name;
+    int batterylevel;
+
+    void charge(){
+        batterylevel = 100;
+        System.out.println("Full charge!!");
+
+    }
+}
+
+
+
+
+public class Main4 {
+    public static void main(String[] args){
+        Sensor s = new Sensor();
+        s.name = "Temp sensor ";
+
+        s.charge();
+        System.out.println("Battery level :" + s.batterylevel);
+
+    }
+    
+}
 
